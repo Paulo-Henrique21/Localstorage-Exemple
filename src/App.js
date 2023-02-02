f6734dd2969021b5d71ddee23a8763a561dbc1e9
Paulo Henrique de Santana de Souza <ph.sant@hotmail.com>
2023-02-01 21:14:43 -0300

    CRIADO EXEMPLO DE SALVAMENTO NO LOCALSTORAGE

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function Teste(){
+
+  const [nome, setNome] = useState()
+
+  /* função para armazenar no localstorage */
+  const armazenar=(chave,valor)=>{
+    localStorage.setItem(chave, valor)
+  }
+
+  /* função para consultar no localstorage */
+  const consultar=(chave)=>{
+    alert(localStorage.getItem(chave))
+  }
+
+  /* função para apagar no localstorage */
+  const apagar=(chave)=>{
+    localStorage.removeItem(chave)
+  }
+  
+  return(
+    <>
+      <label>Digite um nome</label><br/>
+      <input type="text" value={nome} onChange={(e)=>setNome(e.target.value)}/><br/>
+      <button onClick={()=>armazenar('ls_nome', nome)}>Gravar Nome</button>
+      <button onClick={()=>consultar('ls_nome')}>Ver Nome</button>
+      <button onClick={()=>apagar('ls_nome')}>Remover Nome</button>
+    </>
+  )
 }
 
-export default App;
+localStorage.setItem("nome", "Bruno")/* Cria a chave se não existir, se existir ele muda para a nova informação */
+  localStorage.getItem("nome")
+  /* Obtem valor da chave */
+  localStorage.removeItem("nome")
+  /* Remove valor da chave */
